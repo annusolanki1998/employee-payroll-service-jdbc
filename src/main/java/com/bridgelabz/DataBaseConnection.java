b3cfe3c32e16bd83d977cbd130c862787054a4de
Annu <annusinghraj1998@gmail.com>
2022-07-23 20:54:24 +0530

    ADDED: Create payroll service database and connected to java program

--- src/main/java/com/bridgelabz/DataBaseConnection.java
+++ src/main/java/com/bridgelabz/DataBaseConnection.java
@@ -1,10 +1,27 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("welcome to employee payroll data base connection ");
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to employee payroll database connection!");
+        String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service";
+        String userName = "root";
+        String password = "12345";
+        Connection connection = null;
 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+            connection = DriverManager.getConnection(jdbcUrl, userName, password);
 
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            connection.close();
+        }
     }
 }
