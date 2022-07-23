ecbcebc9fabdd9b488ee78d1a904b0d4a413d212
Annu <annusinghraj1998@gmail.com>
2022-07-23 14:49:12 +0530

    ADDED: Welcome to employee payroll database connection

---
+++ src/main/java/com/bridgelabz/DataBaseConnection.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class DataBaseConnection {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee payroll database connection!");
+
+
+    }
+}
